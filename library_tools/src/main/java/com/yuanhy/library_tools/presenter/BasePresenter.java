85b3b1a19eb197ffb2233ebd9921995ee568fb02
yuanhy <375082603@qq.com>
2019-07-10 10:32:23 +0800

    1.0.3忽略节点

--- library_tools/src/main/java/com/yuanhy/library_tools/presenter/BasePresenter.java
+++ library_tools/src/main/java/com/yuanhy/library_tools/presenter/BasePresenter.java
@@ -2,9 +2,11 @@ package com.yuanhy.library_tools.presenter;
 
 
 
+import com.yuanhy.library_tools.rxjava.RxJavaUtil;
+
 import java.lang.ref.WeakReference;
 
-public abstract class BasePresenter<T> {
+public abstract class BasePresenter<T> extends RxJavaUtil {
     public WeakReference<T> weakReference;
    public void onBindView(T view){
        weakReference = new WeakReference<T>(view);
