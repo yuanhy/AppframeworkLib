2ebe9fa5b5e3a092da3498ab8af626d28cb0814c
yuanhy <375082603@qq.com>
2019-07-10 11:55:02 +0800

    1.0.6忽略节点

--- library_tools/src/main/java/com/yuanhy/library_tools/rxjava/RxJavaUtil.java
+++ library_tools/src/main/java/com/yuanhy/library_tools/rxjava/RxJavaUtil.java
@@ -15,7 +15,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public abstract class RxJavaUtil {
     private String tag = "RxjavaUtil";
-    private Disposable disposable;
+    private Disposable disposableInterval;
 
     public static  <T>ObservableTransformer<T,T> io_uiMain(){
         return  new ObservableTransformer<T, T>() {
@@ -55,7 +55,7 @@ public abstract class RxJavaUtil {
 
             @Override
             public void onSubscribe(Disposable d) {
-                disposable = d;
+                disposableInterval = d;
             }
         });
     }
@@ -63,10 +63,10 @@ public abstract class RxJavaUtil {
     /**
      * 取消轮序接口
      */
-    public void cancelDisposable() {
+    public void cancelDisposableInterval() {
         //取消订阅
-        if (disposable != null && !disposable.isDisposed()) {
-            disposable.dispose();
+        if (disposableInterval != null && !disposableInterval.isDisposed()) {
+            disposableInterval.dispose();
         }
     }
 
