bcb5814f4201ea4f1b74718bf760f25f545380bd
yuanhy <375082603@qq.com>
2019-07-15 17:40:22 +0800

    1.0.6忽略节点

--- library_tools/src/main/java/com/yuanhy/library_tools/util/ApiObsoleteUtil.java
+++ library_tools/src/main/java/com/yuanhy/library_tools/util/ApiObsoleteUtil.java
@@ -3,6 +3,7 @@ package com.yuanhy.library_tools.util;
 import android.content.Context;
 import android.os.Build;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * 兼容系统API过时的方法
@@ -21,4 +22,12 @@ public class ApiObsoleteUtil {
             view.setBackgroundDrawable(context.getResources().getDrawable(drawableId));
         }
     }
+    public static void setTextColor(TextView view, Context context, int color){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.setTextColor(context.getColor(color));
+        }else {
+            view.setTextColor(context.getResources().getColor(color));
+        }
+    }
+
 }
